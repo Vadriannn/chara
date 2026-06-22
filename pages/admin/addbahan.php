a1914023447f17ef8d973b2c08aae23ac9cb7834
jeshasa <chrisjere2801@gmail.com>
2026-06-22 11:49:21 +0700

    tambah kkomen

--- pages/admin/addbahan.php
+++ pages/admin/addbahan.php
@@ -74,6 +74,7 @@ try {
 <html lang="en">
   <head>
     <!-- Required meta tags -->
+     <!-- tes git -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title> CHARA - Tambah Bahan</title>
